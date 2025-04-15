c172feebff017cd7424ae67d65793937461ba52b
Alex Nagy <nagyalex003@gmail.com>
2025-04-15 19:40:31 +0200

    feat: enhance data import logic and add cleanup function for old packets

--- vir_conto/vir_conto/doctype/data_packet/data_packet.py
+++ vir_conto/vir_conto/doctype/data_packet/data_packet.py
@@ -5,6 +5,8 @@ import os
 import zipfile
 
 import frappe
+import frappe.utils
+import frappe.utils.logger
 from frappe.model.document import Document
 
 from vir_conto.util import process_dbf
@@ -28,6 +30,9 @@ class datapacket(Document):
 		self.import_data()
 
 	def import_data(self):
+		"""
+		Holding the import logic for CConto export files. It extracts than processes the debase files.
+		"""
 		file_url = os.path.join(frappe.get_site_path("private", "files"), str(self.file_name) + ".LZH")
 		extraction_dir = os.path.join(
 			frappe.get_site_path("private", "files", "storage"), str(self.file_name)
@@ -60,5 +65,33 @@ class datapacket(Document):
 
 				process_dbf(dbf_file, doctype.name, encoding)
 
-		frappe.db.commit()
 		self.is_processed = True
+		self.save()
+		frappe.db.commit()
+
+
+logger = frappe.logger("maintain")
+
+
+def clear_month_older_packets():
+	"""
+	Clearing older than a month (>30 day) packets and files.
+
+	In order to prevent exploding database sizes.
+	"""
+
+	logger.info("Beginning to clean old packets")
+	max_date = frappe.utils.add_days(frappe.utils.getdate(), -30)
+
+	before_delete = frappe.db.count("data-packet")
+	frappe.db.delete("data-packet", {"creation": ["<", max_date]})
+	after_delete = frappe.db.count("data-packet")
+	logger.info(f"Removed {before_delete - after_delete} old packets")
+
+	before_delete = frappe.db.count("File")
+	frappe.db.delete(
+		"File",
+		{"creation": ["<", max_date], "file_name": ["like", "%EI%.LZH"]},
+	)
+	after_delete = frappe.db.count("File")
+	logger.info(f"Removed {before_delete - after_delete} old files")
