e97058f050ea15829257bdc8ed9c26e2c8ac40fb
Alex Nagy <nagyalex003@gmail.com>
2025-04-15 17:32:37 +0200

    feat: add primary-key doctype, refactor migration logic

---
+++ vir_conto/util.py
@@ -0,0 +1,80 @@
+import dbf
+import frappe
+from frappe.model.document import Document
+
+from vir_conto.vir_conto.doctype.primary_key.primary_key import primarykey
+
+
+def process_dbf(dbf_file: str, doctype: str, encoding: str):
+	try:
+		table = dbf.Table(dbf_file, codepage=encoding, on_disk=True)
+		field_names = table.field_names
+		table.open()
+
+		for record in table:
+			row = {}
+			for field_name in field_names:
+				field_info = table.field_info(field_name)
+				if field_info.py_type is str:
+					# Strings are not trimmed by default
+					value = str(record[field_name]).strip()
+				else:
+					value = record[field_name]
+				row[field_name.lower()] = value
+			row["doctype"] = doctype
+
+			if doctype == "torolt":
+				remove_from_db(row)
+			else:
+				insert_into_db(row)
+
+	except dbf.exceptions.DbfError as e:
+		print(e.message)
+
+
+def remove_from_db(row):
+	pass
+	# frappe.db.get_all("primary-key", filters={'type': ['=', 'TERM']})
+	# TODO:
+	# - may need to implement composite keys
+	# frappe.delete_doc(doctype, row["kod"])
+	# 	 if tip='TERM' then
+	#     if findkij(dmf.tblTermek,kod) then abl_term.termek_torol(True);
+	#    if tip='PARTN' then
+	#     if findkij(dmf.tblPartner,kod) then db_muv('D',dmf.tblPartner);
+	#    if tip='TELEP' then
+	#     if findkij(dmf.tblTelep,kod) then db_muv('D',dmf.tblTelep);
+	#    if tip='GVKOD' then
+	#     if findkij(dmf.tblGyujtvk,kod) then db_muv('D',dmf.tblGyujtvk);
+	#    if tip='ARAK' then
+
+
+def get_name(row: dict) -> str:
+	# Selects the primary key for the appropriate doctype
+	pkey: primarykey = frappe.get_doc("primary-key", row["doctype"]).cconto_pkey
+	pkey_list = pkey.split(",")
+	name = ""
+
+	if isinstance(pkey_list, list) and len(pkey_list) > 1:
+		for key in pkey_list:
+			name += row[key] + "/"
+		name = name.rstrip("/")
+	else:
+		name = row[pkey]
+
+	return name
+
+
+def insert_into_db(row: dict) -> None:
+	pkey = get_name(row)
+	doctype = row["doctype"]
+
+	if not frappe.db.exists(doctype, pkey):
+		# create new
+		new_doc = frappe.get_doc(row)
+		new_doc.insert()
+	else:
+		# update
+		old_doc = frappe.get_doc(doctype, pkey)
+		old_doc.update(row)
+		old_doc.save()
--- vir_conto/vir_conto/doctype/primary_key/primary_key.py
+++ vir_conto/vir_conto/doctype/primary_key/primary_key.py
@@ -6,6 +6,22 @@ from frappe.model.document import Document
 
 
 class primarykey(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
 	from typing import TYPE_CHECKING
 
-	pass
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		cconto_name: DF.Data
+		cconto_pkey: DF.Data
+		frappe_name: DF.Data
+		import_order: DF.Int
+		is_enabled: DF.Check
+		is_updateable: DF.Check
+		type: DF.Data | None
+	# end: auto-generated types
+
+	def validate(self):
+		pass
